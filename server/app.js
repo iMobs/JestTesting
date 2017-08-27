2bd15ddcac687aaf86d7abcd1cc4b9be09527c93
Ian Mobley <imobley@mobsworkshop.com>
2017-08-27 16:56:14 -0700

    Add static route to dist folder

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(require('morgan')('dev'));
+
+app.use(express.static(path.resolve(__dirname, '../dist')));
 
 module.exports = app;
