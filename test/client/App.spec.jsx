33ff08e50e93d3559c77b7ac0b2dc85ae961d6b4
Ian Mobley <imobley@mobsworkshop.com>
2017-08-27 17:33:51 -0700

    bring coverage back up using mount

--- test/client/App.spec.jsx
+++ test/client/App.spec.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import App from '../../client/components/App';
 
 describe('<App />', () => {
   it('should render', () => {
-    const app = shallow(<App />);
+    const app = mount(<App />);
 
-    expect(app.find('div').length).toEqual(1);
+    expect(app.find('div').length).toBeGreaterThanOrEqual(1);
   });
 });
