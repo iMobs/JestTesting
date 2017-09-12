eafb72d3829676d5700bf206a2a78ca4962d243f
Ian Mobley <ianmobley@gmail.com>
2017-09-11 20:07:29 -0700

    add wildcard route for server

--- server/app.js
+++ server/app.js
@@ -7,4 +7,20 @@ app.use(require('morgan')('dev'));
 
 app.use(express.static(path.resolve(__dirname, '../dist')));
 
+app.get('/', (req, res) => {
+  res.send(`
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Jest Testing</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
+    </head>
+    <body>
+      <div id="app" />
+      <script type="text/javascript" src="js/bundle.js"></script>
+    </body>
+  </html>
+  `);
+});
+
 module.exports = app;
